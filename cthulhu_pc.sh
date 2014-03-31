17a1e5aa0fc64d368b8c00bcb860abe1874c317a
Stephen Wood <steve@woodathon.com>
2014-03-31 17:57:38 -0400

    modified:   cthulhu_pc.sh

--- cthulhu_pc.sh
+++ cthulhu_pc.sh
@@ -3,6 +3,9 @@
 # this script generates the basic foundation for a CoC 4th edition character
 
 
+# Functions
+
+# roll 3D6
 gener() {
 for i in {0..2}
 do
@@ -12,6 +15,7 @@ do
 done
 }
 
+# roll 2D6+6
 gener_two() {
 for i in {0..1}
 do
@@ -25,9 +29,11 @@ e=$(( d += 6 ))
 }
 
 
+
+# this array contains 7 attributes
 attr=(STR CON POW DEX APP SIZ INT)
 
-
+# print out the values for the 3D6 attributes
 for j in {0..4}
 do
     gener
@@ -36,8 +42,7 @@ do
 done
 
 
-
-
+# print out the values for the 2D6+6 attributes
 for j in {5..6}
 do
     gener_two
